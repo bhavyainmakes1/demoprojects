c8e4b6ed3d62468121a5cfa2f22f61fadc10f48d
bhavyainmakes1 <bhavya.inmakes1@gmail.com>
2022-03-03 02:42:56 +0530

    first

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,26 @@
+'''Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))'''
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('',views.add,name="home"),
+    #path('details',views.details,name='details'),
+    path('delete/<int:task_id>/',views.delete,name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.Taskdetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.Taskdeleteview.as_view(),name='cbvdelete')
+]
